23e81e136602efa91b5ee1e30a263eb3f9ac2cdb
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-23 16:38:07 -0300

    creating Todos model for project with props by Equatable to help in tests

---
+++ lib/services/json_placeholder/models/todos_model.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+class Todos extends Equatable {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  const Todos(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.completed});
+
+  factory Todos.fromJson(Map<String, dynamic> json) {
+    return Todos(
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title'],
+        completed: json['completed']);
+  }
+
+  @override
+  List<Object?> get props => [userId, id, title, completed];
+}
