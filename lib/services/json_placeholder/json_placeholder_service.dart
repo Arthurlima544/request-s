b4827eed4160c029446c3e549ad3d538ff4e5bd3
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-23 16:45:13 -0300

    add service with our client Interface on contructor this service will return a list off todos

---
+++ lib/services/json_placeholder/json_placeholder_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:learn_more_about_request/services/json_placeholder/interface/http_client_interface.dart';
+import 'models/todos_model.dart';
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+class JsonPlaceholderService {
+  final HttpClientInterface client;
+
+  JsonPlaceholderService(this.client);
+
+  Future<List<Todos>> getAll() async {
+    var response = await client.get(url);
+    var body = (response.data as List);
+    List<Todos> listTodos = body.map((item) {
+      return Todos.fromJson(item);
+    }).toList();
+    return listTodos;
+  }
+}
