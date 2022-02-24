53f50b93337be09b0dc7c96f0d88d4fc47f8c6fc
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-24 11:08:13 -0300

    updateing view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learn_more_about_request/view/controller/list_todos_controller.dart';
+
+import 'services/json_placeholder/models/todos_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,14 +19,69 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ListTodosController controller = ListTodosController();
+
+  @override
+  void initState() {
+    controller.list.addListener(() {
+      setState(() {});
+    });
+    controller.getTodos();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hello"),
+        title: const Text("List Todos"),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: controller.list,
+        builder: (context, List<Todos> value, child) {
+          return ListView.builder(
+              itemCount: value.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "${value[index].id}",
+                          overflow: TextOverflow.clip,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Text(
+                          value[index].title,
+                          overflow: TextOverflow.clip,
+                        ),
+                      ),
+                      Expanded(
+                        child: value[index].completed
+                            ? const Icon(
+                                Icons.check,
+                                color: Colors.green,
+                              )
+                            : const Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
       ),
     );
   }
