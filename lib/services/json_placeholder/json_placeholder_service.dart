99d3e6980d24d56b9d282e52519f04f4d23d6281
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-23 21:40:17 -0300

    add base url and method postTodos services

--- lib/services/json_placeholder/json_placeholder_service.dart
+++ lib/services/json_placeholder/json_placeholder_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:learn_more_about_request/services/json_placeholder/interface/http_client_interface.dart';
 import 'models/todos_model.dart';
 
-const url = "https://jsonplaceholder.typicode.com/todos";
+const baseUrl = "https://jsonplaceholder.typicode.com";
 
 class JsonPlaceholderService {
   final HttpClientInterface client;
@@ -10,11 +10,17 @@ class JsonPlaceholderService {
   JsonPlaceholderService(this.client);
 
   Future<List<Todos>> getTodos() async {
-    var response = await client.get(url);
+    var response = await client.get("$baseUrl/todos");
     var body = (response.data as List);
     List<Todos> listTodos = body.map((item) {
       return Todos.fromJson(item);
     }).toList();
     return listTodos;
+  }
+
+  Future<Todos> postTodo(Todos todo) async {
+    Response response = await client.post("$baseUrl/posts", todo.toJson());
+    var newTodo = Todos.fromJson(response.data);
+    return newTodo;
   }
 }
